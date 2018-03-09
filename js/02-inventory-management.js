8ebca05a8beb91eb16272e9d97d97471cf6245e8
Alex Stolzoff <amstolzo@ucsd.edu>
2018-03-08 18:48:43 -0800

    done

--- js/02-inventory-management.js
+++ js/02-inventory-management.js
@@ -1,5 +1,5 @@
 /*eslint-env browser*/
-
+var storage;
 
 function showMenu() {
     "use strict";
@@ -14,98 +14,112 @@ function showMenu() {
 
 function viewAllProducts(inventory) {
     "use strict";
-    
-    var sku1, sku2, sku3, sku4, sku5, product1, product2, product3, product4, product5, quantity1, quantity2, quantity3, quantity4, quantity5, cost1, cost2, cost3, cost4, cost5;
-    sku1 = inventory[0][0][1];
-    sku2 = inventory[1][0][1];
-    sku3 = inventory[2][0][1];
-    sku4 = inventory[3][0][1];
-    sku5 = inventory[4][0][1];
-
-    product1 = inventory[0][1][1];
-    product2 = inventory[1][1][1];
-    product3 = inventory[2][1][1];
-    product4 = inventory[3][1][1];
-    product5 = inventory[4][1][1];
-
-    quantity1 = "(" + inventory[0][2][1] + ")";
-    quantity2 = "(" + inventory[1][2][1] + ")";
-    quantity3 = "(" + inventory[2][2][1] + ")";
-    quantity4 = "(" + inventory[3][2][1] + ")";
-    quantity5 = "(" + inventory[4][2][1] + ")";
-
-    cost1 = "$" + inventory[0][3][1];
-    cost2 = "$" + inventory[1][3][1];
-    cost3 = "$" + inventory[2][3][1];
-    cost4 = "$" + inventory[3][3][1];
-    cost5 = "$" + inventory[4][3][1];
-
-    var firstItem = [sku1, product1, quantity1, cost1],
-        secondItem = [sku2, product2, quantity2, cost2],
-        thirdItem = [sku3, product3, quantity3, cost3],
-        fourthItem = [sku4, product4, quantity4, cost4],
-        fifthItem = [sku5, product5, quantity5, cost5],
-        skuArray = [firstItem, secondItem, thirdItem, fourthItem, fifthItem];
+    storage = localStorage.getItem("array");
 
 
+    if (storage !== null) {
+        var newSku1, newSku2, newSku3, newSku4, newSku5, newProduct1, newProduct2, newProduct3, newProduct4, newProduct5, newQuantity1, newQuantity2, newQuantity3, newQuantity4, newQuantity5, newCost1, newCost2, newCost3, newCost4, newCost5, newItem1, newItem2, newItem3, newItem4, newItem5, finalArray;
+        inventory = [storage.split(",")];
 
-//    if (storage === null) {
+        //        window.console.log(typeof inventory);
+        window.console.log(inventory);
+        window.console.log(inventory.length);
+        newSku1 = inventory[0][1];
+        newSku2 = inventory[0][9];
+        newSku3 = inventory[0][17];
+        newSku4 = inventory[0][25];
+        newSku5 = inventory[0][33];
+
+        newProduct1 = inventory[0][3];
+        newProduct2 = inventory[0][11];
+        newProduct3 = inventory[0][19];
+        newProduct4 = inventory[0][27];
+        newProduct5 = inventory[0][35];
+
+        newQuantity1 = "(" + inventory[0][5] + ")";
+        newQuantity2 = "(" + inventory[0][13] + ")";
+        newQuantity3 = "(" + inventory[0][21] + ")";
+        newQuantity4 = "(" + inventory[0][29] + ")";
+        newQuantity5 = "(" + inventory[0][37] + ")";
+
+        newCost1 = "$" + inventory[0][7];
+        newCost2 = "$" + inventory[0][15];
+        newCost3 = "$" + inventory[0][23];
+        newCost4 = "$" + inventory[0][31];
+        newCost5 = "$" + inventory[0][39];
+
+        newItem1 = [newSku1, newProduct1, newQuantity1, newCost1];
+        newItem2 = [newSku2, newProduct2, newQuantity2, newCost2];
+        newItem3 = [newSku3, newProduct3, newQuantity3, newCost3];
+        newItem4 = [newSku4, newProduct4, newQuantity4, newCost4];
+        newItem5 = [newSku5, newProduct5, newQuantity5, newCost5];
+
+        finalArray = [newItem1, newItem2, newItem3, newItem4, newItem5];
+
+        finalArray.sort();
+
+
+        window.console.log(finalArray[0].join(" "));
+        window.console.log(finalArray[1].join(" "));
+        window.console.log(finalArray[2].join(" "));
+        window.console.log(finalArray[3].join(" "));
+        window.console.log(finalArray[4].join(" "));
+
+
+
+
+    } else if (storage === null) {
+        var sku1, sku2, sku3, sku4, sku5, product1, product2, product3, product4, product5, quantity1, quantity2, quantity3, quantity4, quantity5, cost1, cost2, cost3, cost4, cost5;
+
+        sku1 = inventory[0][0][1];
+        sku2 = inventory[1][0][1];
+        sku3 = inventory[2][0][1];
+        sku4 = inventory[3][0][1];
+        sku5 = inventory[4][0][1];
+
+        product1 = inventory[0][1][1];
+        product2 = inventory[1][1][1];
+        product3 = inventory[2][1][1];
+        product4 = inventory[3][1][1];
+        product5 = inventory[4][1][1];
+
+        quantity1 = "(" + inventory[0][2][1] + ")";
+        quantity2 = "(" + inventory[1][2][1] + ")";
+        quantity3 = "(" + inventory[2][2][1] + ")";
+        quantity4 = "(" + inventory[3][2][1] + ")";
+        quantity5 = "(" + inventory[4][2][1] + ")";
+
+        cost1 = "$" + inventory[0][3][1];
+        cost2 = "$" + inventory[1][3][1];
+        cost3 = "$" + inventory[2][3][1];
+        cost4 = "$" + inventory[3][3][1];
+        cost5 = "$" + inventory[4][3][1];
+
+        var firstItem = [sku1, product1, quantity1, cost1],
+            secondItem = [sku2, product2, quantity2, cost2],
+            thirdItem = [sku3, product3, quantity3, cost3],
+            fourthItem = [sku4, product4, quantity4, cost4],
+            fifthItem = [sku5, product5, quantity5, cost5],
+            skuArray = [firstItem, secondItem, thirdItem, fourthItem, fifthItem];
+
+
         skuArray = skuArray.sort();
-        for (var i in skuArray) 
-            window.console.log(skuArray[i].join(" ").toString());
-        
-//    } else {
-//        storage = localStorage.getItem("array");
-////        skuArray = storage;
-////        sku1 = storage[0][0][1];
-////    sku2 = storage[1][0][1];
-////    sku3 = storage[2][0][1];
-////    sku4 = storage[3][0][1];
-////    sku5 = storage[4][0][1];
-////
-////    product1 = storage[0][1][1];
-////    product2 = storage[1][1][1];
-////    product3 = storage[2][1][1];
-////    product4 = storage[3][1][1];
-////    product5 = storage[4][1][1];
-////
-////    quantity1 = "(" + storage[0][2][1] + ")";
-////    quantity2 = "(" + storage[1][2][1] + ")";
-////    quantity3 = "(" + storage[2][2][1] + ")";
-////    quantity4 = "(" + storage[3][2][1] + ")";
-////    quantity5 = "(" + storage[4][2][1] + ")";
-////
-////    cost1 = "$" + storage[0][3][1];
-////    cost2 = "$" + storage[1][3][1];
-////    cost3 = "$" + storage[2][3][1];
-////    cost4 = "$" + storage[3][3][1];
-////    cost5 = "$" + storage[4][3][1];
-////
-////        firstItem = [sku1, product1, quantity1, cost1],
-////        secondItem = [sku2, product2, quantity2, cost2],
-////        thirdItem = [sku3, product3, quantity3, cost3],
-////        fourthItem = [sku4, product4, quantity4, cost4],
-////        fifthItem = [sku5, product5, quantity5, cost5],
-////        skuArray = [firstItem, secondItem, thirdItem, fourthItem, fifthItem];
-////        window.console.log(skuArray);
-////        skuArray = skuArray;
-////        window.console.log(skuArray[0].join(" "). toString());
-////        for (var x in skuArray) {
-////            window.console.log(skuArray[x].toString());
-////        }
-//    }
+
+        window.console.log(skuArray[0].join(" ").toString());
+        window.console.log(skuArray[1].join(" ").toString());
+        window.console.log(skuArray[2].join(" ").toString());
+        window.console.log(skuArray[3].join(" ").toString());
+        window.console.log(skuArray[4].join(" ").toString());
+
+    }
+
 
 }
 
 
 function updateSkuNumber(array) {
     "use strict";
-    var skuNumber = Number(window.prompt("Please enter sku number"));
-    var sku1 = array[0][0][1];
-    var sku2 = array[1][0][1];
-    var sku3 = array[2][0][1];
-    var sku4 = array[3][0][1];
-    var sku5 = array[4][0][1];
+    var skuNumber = Number(window.prompt("Please enter sku number")), sku1 = array[0][0][1], sku2 = array[1][0][1], sku3 = array[2][0][1], sku4 = array[3][0][1], sku5 = array[4][0][1];
     window.console.log(skuNumber, sku1, sku2, sku3, sku4, sku5);
     if (skuNumber !== null) {
         if (skuNumber === sku1) {
@@ -144,7 +158,7 @@ function updateSkuNumber(array) {
 
             }
         }
-        window.console.log(array);
+
 
 
     }
